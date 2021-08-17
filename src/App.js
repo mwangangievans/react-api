41213562e3c681d63f9536001296f25d3c33d38d
root <root@kali.kali>
2021-08-17 05:53:08 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 import './App.css';
+import Create from './components/create';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Router>
+  <div className="main">
+    <h2 className="main-header"> React Crud Operations</h2>
+    <div>
+    <Create />
     </div>
+   </div>
+    </Router>
+ 
   );
 }
 
---
+++ src/components/create.js
@@ -0,0 +1,36 @@
+
+import React , {useState} from 'react'
+import axios from 'axios';
+import {Button ,Checkbox ,Form, FormInput }  from  'semantic-ui-react'
+
+const Create = () => {
+    const [firstName ,setFirstName] = useState('');
+    const [LastName, setLastName ] =useState('');
+    const [checkbox , setCheckbox ]= useState(false);
+
+    const postData = () => {
+
+        axios.post(`https://611b43d822020a00175a43ce.mockapi.io/fakedata`,{
+            firstName,
+            LastName,
+            checkbox
+        });
+    }
+    return (
+        <Form className="create-form" >
+            <Form.Field>
+                <label>First Name</label>
+                <input placeholder="First Name" onChange={(e) => setFirstName(e.target.value)}/>
+            </Form.Field>
+            <Form.Field>
+                <label>Last Name</label>
+                <input placeholder="Last Name" onChange={(e) => setLastName(e.target.value)}/>
+            </Form.Field>
+            <Form.Field>
+                <Checkbox label="I agree to the Terms and Conditions" onChange={(e) => setCheckbox(!checkbox)}/>
+            </Form.Field>
+           <button  onClick={postData} type="submit">Submit</button>
+        </Form>
+    );
+}
+export default Create 
